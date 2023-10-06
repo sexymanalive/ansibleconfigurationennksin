a7bc7f772a181a4bfb5f0ac75e23052f42f2a1de
Keo  Kay <keokay888@gmail.com>
2023-10-06 15:47:10 +0700

    configuration updates

--- ansible/shellscript/testing.sh
+++ ansible/shellscript/testing.sh
@@ -1,8 +1,13 @@
- #!/bin/bash
-echo "======<<Hello Shellscript>>====== " 
-mkdir /home/ubuntu/configuration
-cd /home/ubuntu
-touch article1.txt 
-touch article2.txt 
+#!/bin/bash
 
-echo "======<<Hello Shellscript>>====== " > article1.txt
+echo "======<<Hello Shellscript>>======"
+
+mkdir -p /home/ubuntu/configuration
+cd /home/ubuntu/configuration
+
+touch /home/ubuntu/article1.txt
+touch /home/ubuntu/article2.txt
+
+echo "======<<Hello Shellscript>>======" > /home/ubuntu/article1.txt
+
+echo "Script executed successfully!"
