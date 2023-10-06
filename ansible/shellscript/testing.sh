c961c4b0d81ae48b2b9e08079c4c29ee8e2482d6
Keo  Kay <keokay888@gmail.com>
2023-10-06 16:01:14 +0700

    configuration updates

--- ansible/shellscript/testing.sh
+++ ansible/shellscript/testing.sh
@@ -7,3 +7,6 @@ sudo sh get-docker.sh
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Add permission for current user in docker 
+sudo usermod -aG docker $USER
